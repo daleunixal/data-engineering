1a4b70c48e06fb3ea3c506b186b32ee2196e0f59
axel <axelwhalefort@gmail.com>
2023-11-16 02:41:46 +0500

    Fix second practice

--- pr2/task1.py
+++ pr2/task1.py
@@ -11,12 +11,19 @@ matrix_stat['sum'] = np.sum(matrix)
 matrix_stat['avr'] = matrix_stat['sum'] / (size * size)
 matrix_stat['sumMD'] = np.trace(matrix)
 matrix_stat['avrMD'] = matrix_stat['sumMD'] / matrix.shape[0]
-matrix_stat['sumSD'] = np.trace(matrix, offset=1)
-matrix_stat['avrSD'] = matrix_stat['sumSD'] / matrix.shape[0]
+matrix_stat['sumSD'] = 0
+matrix_stat['avrSD'] = 0
 matrix_stat['max'] = matrix.max()
 matrix_stat['min'] = matrix.min()
 
-with open('./result/matrix_stat.json', 'w') as result:
+for i in range(0, size):
+    for j in range(0, size):
+        if i + j == (size - 1):
+            matrix_stat['sumSD'] += matrix[i][j]
+
+matrix_stat['avrSD'] = matrix_stat['sumSD'] / size
+
+with open('../result/matrix_stat.json', 'w') as result:
     result.write(json.dumps(matrix_stat))
 
 norm_matrix = np.ndarray((size, size), dtype=float)
@@ -25,4 +32,5 @@ for i in range(0, size):
     for j in range(0, size):
         norm_matrix[i][j] = matrix[i][j] / matrix_stat['sum']
 
-np.save("./result/norm_matrix.npy", norm_matrix)
+np.save("../result/norm_matrix.npy", norm_matrix)
+
--- pr2/task5.py
+++ pr2/task5.py
@@ -4,7 +4,6 @@ from scipy import stats
 import msgpack
 import pandas as pd
 
-
 # Работаем с полями
 # channelViewCount: float
 # elapsedtime: float
@@ -15,14 +14,17 @@ import pandas as pd
 # videoDislikeCount: float
 # channelelapsedtime: float
 
-channelViewCount_list = []
-elapsedtime_list = []
-videoCategoryId_list = []
-videoCount_list = []
-videoViewCount_list = []
-videoLikeCount_list = []
-videoDislikeCount_list = []
-channelelapsedtime_list = []
+lookup_list = ['channelViewCount',
+               'elapsedtime',
+               'videoCategoryId',
+               'videoCount',
+               'videoViewCount',
+               'videoLikeCount',
+               'videoDislikeCount',
+               'channelelapsedtime',
+               ]
+
+data_dict = {}
 
 dataset = []
 
@@ -30,93 +32,30 @@ with open('./ytb.json', 'r', encoding='utf-8') as inp:
     dataset = json.load(inp)
 
 for item in dataset:
-    channelViewCount_list.append(item['channelViewCount'])
-    elapsedtime_list.append(item['elapsedtime'])
-    videoCategoryId_list.append(item['videoCategoryId'])
-    videoCount_list.append(item['videoCount'])
-    videoViewCount_list.append(item['videoViewCount'])
-    videoLikeCount_list.append(item['videoLikeCount'])
-    videoDislikeCount_list.append(item['videoDislikeCount'])
-    channelelapsedtime_list.append(item['channelelapsedtime'])
+    for key in item.keys():
+        if key not in lookup_list:
+            continue
+        if key not in data_dict:
+            data_dict[key] = []
+        data_dict[key].append(item[key])
 
-channelViewCount_list = list(filter(lambda x: x is not None, channelViewCount_list))
-elapsedtime_list = list(filter(lambda x: x is not None, elapsedtime_list))
-videoCategoryId_list = list(filter(lambda x: x is not None, videoCategoryId_list))
-videoCount_list = list(filter(lambda x: x is not None, videoCount_list))
-videoViewCount_list = list(filter(lambda x: x is not None, videoViewCount_list))
-videoLikeCount_list = list(filter(lambda x: x is not None, videoLikeCount_list))
-videoDislikeCount_list = list(filter(lambda x: x is not None, videoDislikeCount_list))
-channelelapsedtime_list = list(filter(lambda x: x is not None, channelelapsedtime_list))
 
-channelViewCount_list = list(map(lambda x: float(x), channelViewCount_list))
-elapsedtime_list = list(map(lambda x: float(x), elapsedtime_list))
-videoCategoryId_list = list(map(lambda x: float(x), videoCategoryId_list))
-videoCount_list = list(map(lambda x: float(x), videoCount_list))
-videoViewCount_list = list(map(lambda x: float(x), videoViewCount_list))
-videoLikeCount_list = list(map(lambda x: float(x), videoLikeCount_list))
-videoDislikeCount_list = list(map(lambda x: float(x), videoDislikeCount_list))
-channelelapsedtime_list = list(map(lambda x: float(x), channelelapsedtime_list))
+def process_metric_by_artifact(data_list: [], metric_name: str):
+    filtered_list = list(filter(lambda x: x is not None, data_list))
+    mapped_list = list(map(lambda x: float(x), filtered_list))
 
-result = []
+    return {
+        metric_name: {
+            'avg': mean(mapped_list),
+            'min': min(mapped_list),
+            'max': max(mapped_list),
+            'sum': sum(mapped_list),
+            'std_err': stats.sem(mapped_list)
+        }
+    }
 
-result.append({
-    'channelViewCount': {
-        'avg': mean(channelViewCount_list),
-        'min': min(channelViewCount_list),
-        'max': max(channelViewCount_list),
-        'sum': sum(channelViewCount_list),
-        'std_err': stats.sem(channelViewCount_list)
-    },
-    'elapsedtime': {
-        'avg': mean(elapsedtime_list),
-        'min': min(elapsedtime_list),
-        'max': max(elapsedtime_list),
-        'sum': sum(elapsedtime_list),
-        'std_err': stats.sem(elapsedtime_list)
-    },
-    'videoCategoryId': {
-        'avg': mean(videoCategoryId_list),
-        'min': min(videoCategoryId_list),
-        'max': max(videoCategoryId_list),
-        'sum': sum(videoCategoryId_list),
-        'std_err': stats.sem(videoCategoryId_list)
-    },
-    'videoCount': {
-        'avg': mean(videoCount_list),
-        'min': min(videoCount_list),
-        'max': max(videoCount_list),
-        'sum': sum(videoCount_list),
-        'std_err': stats.sem(videoCount_list)
-    },
-    'videoViewCount': {
-        'avg': mean(videoViewCount_list),
-        'min': min(videoViewCount_list),
-        'max': max(videoViewCount_list),
-        'sum': sum(videoViewCount_list),
-        'std_err': stats.sem(videoViewCount_list)
-    },
-    'videoLikeCount': {
-        'avg': mean(videoLikeCount_list),
-        'min': min(videoLikeCount_list),
-        'max': max(videoLikeCount_list),
-        'sum': sum(videoLikeCount_list),
-        'std_err': stats.sem(videoLikeCount_list)
-    },
-    'videoDislikeCount': {
-        'avg': mean(videoDislikeCount_list),
-        'min': min(videoDislikeCount_list),
-        'max': max(videoDislikeCount_list),
-        'sum': sum(videoDislikeCount_list),
-        'std_err': stats.sem(videoDislikeCount_list)
-    },
-    'channelelapsedtime': {
-        'avg': mean(channelelapsedtime_list),
-        'min': min(channelelapsedtime_list),
-        'max': max(channelelapsedtime_list),
-        'sum': sum(channelelapsedtime_list),
-        'std_err': stats.sem(channelelapsedtime_list)
-    },
-})
+
+result = list(map(lambda x: process_metric_by_artifact(data_dict[x], x), list(data_dict.keys())))
 
 with open('./task5_r.json', 'w', encoding='utf-8') as out:
     out.write(json.dumps(result))
@@ -124,10 +63,14 @@ with open('./task5_r.json', 'w', encoding='utf-8') as out:
 with open('./task5_r.msgpack', 'wb') as out:
     out.write(msgpack.packb(result))
 
-df = pd.DataFrame([x for x in result])
+df_obj = {}
+
+for dict in result:
+    df_obj.update(dict)
+
+df = pd.DataFrame([df_obj])
 
 with open('./task5_r.csv', 'w', encoding='utf-8') as out:
     out.write(df.to_csv(index=False))
 
 df.to_pickle('./task5_r.pkl')
-
